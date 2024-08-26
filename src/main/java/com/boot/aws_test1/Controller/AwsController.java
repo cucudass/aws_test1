a96e7078ebd9859d9e60745615e7b4117d6a0b9f
cucudass <whrhkdfo12@naver.com>
2024-08-26 15:02:12 +0900

    add all

--- src/main/java/com/boot/aws_test1/Controller/AwsController.java
+++ src/main/java/com/boot/aws_test1/Controller/AwsController.java
@@ -1,15 +1,15 @@
-package Controller;
+package com.boot.aws_test1.Controller;
 
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
-@Controller
+@RestController
 @Slf4j
 public class AwsController {
     @GetMapping("/aws/v1")
     public String hello() {
-        log.info("/aws/v1");
+        log.info("/aws/v1 호출");
 
         return "<h1>aws v1 호출</h1>";
     }
